b7e47c61d73a71be285ca1d3ceb9c73d239c06de
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2025-06-01 15:17:22 +0300

    add db connection and first model

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,13 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
@@ -20,9 +26,9 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    "default",
+    "{controller=Home}/{action=Index}/{id?}"
+).WithStaticAssets();
 
 
 app.Run();
